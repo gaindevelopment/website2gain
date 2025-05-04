d7528843cd86e0313b58aee8d4d5cc11bc7f1722
gaindevelopment <darkstaff74@gmail.com>
2025-05-04 21:27:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+﻿const search = document.getElementById("search");
+const list = document.getElementById("site-list");
+
+const sites = [
+  // Paste your URLs here
+  "https://swagbucks.com",
+  "https://timebucks.com",
+  "https://faucetpay.io",
+  // ... (I'll paste all 200+ here for you)
+];
+
+function renderList(filter = "") {
+  list.innerHTML = "";
+  sites
+    .filter(site => site.toLowerCase().includes(filter.toLowerCase()))
+    .forEach(site => {
+      const li = document.createElement("li");
+      li.innerHTML = `<a href="${site}" target="_blank">${site}</a>`;
+      list.appendChild(li);
+    });
+}
+
+search.addEventListener("input", () => {
+  renderList(search.value);
+});
+
+renderList();
